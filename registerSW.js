1556db25a317f88e27e12e01ff5f18dc2ba2767f
pschuermann <pschuermann@users.noreply.github.com>
2026-02-01 07:57:13 +0000

    deploy: pschuermann/rage-against-the-campervan-source@d7f44d2f09ee891002ff35ad6efe447b287c92a6

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/rage-against-the-campervan/sw.js', { scope: '/rage-against-the-campervan/' })})}
